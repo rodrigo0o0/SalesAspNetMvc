0412eea8f1881232f00b78cde4f12071221afb01
Rodrigo Sandeski <sandeski47@gmail.com>
2022-05-04 04:40:56 -0300

    first commit

---
+++ SalesAspNetMvc/Models/ErrorViewModel.fs
@@ -0,0 +1,9 @@
+namespace SalesAspNetMvc.Models
+
+open System
+
+type ErrorViewModel =
+    { RequestId: string }
+
+    member this.ShowRequestId =
+        not (String.IsNullOrEmpty(this.RequestId))
